78ffa63fe3ece3666530406eafb2a99d3b687009
2023zy <126959137+2023zy@users.noreply.github.com>
2025-04-17 10:21:43 +0800

    Update some_commands.py
    solve errors due to the id setting of target classes (start from 1 instead of 0).

--- rtdetr_pytorch/some_commands.py
+++ rtdetr_pytorch/some_commands.py
@@ -7,3 +7,4 @@ CUDA_VISIBLE_DEVICES=9 torchrun --master_port=9909 --nproc_per_node=1 tools/trai
 inference code to enable folder image visualization:
 (rtdetr) zy@NIPS:/data1/zy/code/rt_detr/rtdetr_pytorch$ python tools/torch_inf.py -c configs/rtdetr/rtdetr_r50vd_6x_coco.yml -r output/rtdetr_r50vd_6x_coco_copper_24e_1/checkpoint0020.pth --input /data1/zy/dataset/cooper001/newsplit/coco/test2017/ --device cuda:0 --output test_vis
 
+ps. if the ids of target classes start from 1 instead of 0, the rectification should be made the same as that of the matcher.py in D-FINE.
